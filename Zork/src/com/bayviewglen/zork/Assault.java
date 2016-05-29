17cb059a48fc47416655e911d36016d768fba6a8
Gregory Rochlitz <grochlitz@bayviewglen.ca>
2016-05-28 21:03:55 -0400

    Added Henchmen class and did percent hits for Assault class. In Assault there are still some sections that do not work, so whoever can, please look over and fix those when available. (Apparently John did not get this push so I am pushing again.

--- Zork/src/com/bayviewglen/zork/Assault.java
+++ Zork/src/com/bayviewglen/zork/Assault.java
@@ -1,17 +1,143 @@
 package com.bayviewglen.zork;
 
-public abstract class Assault {
+import java.util.concurrent.ThreadLocalRandom;
 
+public abstract class Assault {
+	
+	public static int maxPercent = 100;
+	public static int gunHitRate = 100;
+	public static int knifeHitRate;
+	public static int swordHitRate;
+	public static int crossbowHitRate;
+
+	//this method is assuming that the player has already typed "attack zombie with ___"
 	public static void attackZombie(Room currentRoom, Player user, Command command) {
 		Character x = currentRoom.getRoster().getCharacterString("zombie");
-		currentRoom.getRoster().removeCharacter(x);
-		System.out.println("Slain.");
+		if (currentRoom.getRoster().hasCharacter(x)) {
+			if (command.getFourthWord() == "knife") {
+				if (user.getInventory().hasItem("knife")) {
+					knifeHitRate = 40;
+					int sucessHit = (int) Math.random() * maxPercent;
+					if (sucessHit <= knifeHitRate) {
+						currentRoom.getRoster().removeCharacter(x);
+						System.out.println("You killed the zombie.");
+					}else {
+						int damageDone = zombie.zombieDamage();
+						user.removeHealth(damageDone);
+						System.out.println("You missed and the zombie dealt " + damageDone + " damage");
+					}
+				}else {
+					System.out.println("You don't have a knife...");
+					//should you take damage here if you attack with something you don't have?
+					System.out.println("What would you like to do now?");
+				}
+			}else if (command.getFourthWord() == "sword") {
+				if (user.getInventory().hasItem("sword")) {
+					swordHitRate = 60;
+					int sucessHit = (int) Math.random() * maxPercent;
+					if (sucessHit <= swordHitRate) {
+						currentRoom.getRoster().removeCharacter(x);
+						System.out.println("You killed the zombie.");
+					}else {
+						int damageDone = zombie.zombieDamage();
+						user.removeHealth(damageDone);
+						System.out.println("You missed and the zombie dealt " + damageDone + " damage");
+					}
+				}else {
+					System.out.println("You don't have a sword...");
+					//see last comment
+					System.out.println("What would you like to do now?");
+				}
+			}else if (command.getFourthWord() == "crossbow") {
+				if (user.getInventory().hasItem("crossbow")) {
+					currentRoom.getRoster().removeCharacter(x);
+					System.out.println("You killed the zombie.");
+					}else {
+					System.out.println("You don't have a crossbow...");
+					//see last comment
+					System.out.println("What would you like to do now?");
+				}
+			}else if (command.getFourthWord() == "gun" || command.getFourthWord() == "handgun") {
+				if (user.getInventory().hasItem("gun")) {
+					currentRoom.getRoster().removeCharacter(x);
+					System.out.println("You killed the zombie.");
+					}else {
+					System.out.println("You don't have a gun...");
+					//see last comment
+					System.out.println("What would you like to do now?");
+				}
+			}
+		}else {
+			System.out.println("There are no zombies here...");
+		}
 	}
-
+	
+	//this method is assuming that the player has already typed "attack henchman with ___"
 	public static void attackHenchman(Room currentRoom, Player user, Command command) {
 		Character x = currentRoom.getRoster().getCharacterString("henchman");
-		currentRoom.getRoster().removeCharacter(x);
-		System.out.println("Slain.");
+		if (currentRoom.getRoster().hasCharacter(x)) {
+			if (command.getFourthWord() == "knife") {
+				if (user.getInventory().hasItem("knife")) {
+					knifeHitRate = 30;
+					int sucessHit = (int) Math.random() * maxPercent;
+					if (sucessHit <= knifeHitRate) {
+						int damageDone = henchmen.henchmenDamage();
+						user.removeHealth(damageDone);
+						currentRoom.getRoster().removeCharacter(x);
+						System.out.println("You killed the henchman, but took " + damageDone + " damage.");
+						}else {
+						user.kill();
+						System.out.println("You missed and the henchman killed you.");
+					}
+				}else {
+					System.out.println("You don't have a knife...");
+					//should you die if you attack with weapon but don't have that weapon?
+					System.out.println("What would you like to do now?");
+				}
+			}else if (command.getFourthWord() == "sword") {
+				if (user.getInventory().hasItem("sword")) {
+					swordHitRate = 50;
+					int sucessHit = (int) Math.random() * maxPercent;
+					if (sucessHit <= swordHitRate) {
+						currentRoom.getRoster().removeCharacter(x);
+						System.out.println("You killed the henchman.");
+						}else {
+						user.kill();
+						System.out.println("You missed and the henchman killed you.");
+					}
+			}else {
+				System.out.println("You don't have a sword...");
+				System.out.println("What would you like to do now?");
+			}
+			}else if (command.getFourthWord() == "crossbow") {
+				if (user.getInventory().hasItem("crossbow")) {
+					crossbowHitRate = 70;
+					int sucessHit = (int) Math.random() * maxPercent;
+					if (sucessHit <= crossbowHitRate) {
+						currentRoom.getRoster().removeCharacter(x);
+						System.out.println("You killed the henchman.");
+						}else {
+						int damageDone = henchmen.henchmenDamage();
+						user.removeHealth(damageDone);
+						System.out.println("You missed and the henchman did " + damageDone + " damage.");
+					}
+				}else {
+					System.out.println("You don't have a crossbow...");
+					System.out.println("What would you like to do now?");
+				}
+			}else if (command.getFourthWord() == "gun" || command.getFourthWord() == "handgun") {
+				if (user.getInventory().hasItem("gun")) {
+					currentRoom.getRoster().removeCharacter(x);
+					System.out.println("You killed the henchman");
+				}
+			}else {
+				System.out.println("You can't attack the henchman with that!");
+				System.out.println("What do you want to do now?");
+			}
+		}else {
+			System.out.println("There are no henchmen here...");
+		}
 	}
+	
 
 }
---
+++ Zork/src/com/bayviewglen/zork/Henchmen.java
@@ -0,0 +1,26 @@
+package com.bayviewglen.zork;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+//wasn't really sure what to do for this one...
+public class Henchmen extends Character{
+	public String[] instantiatePhrases = {"Hey! What are you doing here, kid?", "Hey! Who's that??", "I see someone!", 
+			"An intruder! Get him!", "Someone's here!", "We've been infiltrated!", "Hey! You dare challenge the Saviours?",
+			"Hey! Prepare to face the might of the Saviours!", "I see you!", "An intruder! Show him no mercy!", 
+			"Hey you! Let's negotiate. You conede, and i'll definitely let you live."};
+	
+	public String randomPhrase() { //returns one of the phrases above - use this method when a room with any number of henchmen is entered.
+		int phraseIndex = ThreadLocalRandom.current().nextInt(0, instantiatePhrases.length);
+		return instantiatePhrases[phraseIndex];
+	}
+	
+	public int henchmenDamage(){
+		final int MIN_DAMAGE = 6;
+		final int MAX_DAMAGE = 30;
+		
+		int damage = ThreadLocalRandom.current().nextInt(MIN_DAMAGE, MAX_DAMAGE);
+		return damage;
+	}
+	
+
+}
